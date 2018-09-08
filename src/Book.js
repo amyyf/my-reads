d67f86b678ba9c51b8547c39fa074dc33deea18b
Amy Frieson <ava447@gmail.com>
2018-09-08 09:40:16 -0400

    refactor: Render Book.js dynamically

--- src/Book.js
+++ src/Book.js
@@ -7,11 +7,11 @@ class Book extends React.Component {
   }
 
   render () {
-    const {title, cover, url} = this.props;
+    const {title, authors, imageLinks: {smallThumbnail: img}} = this.props;
     return (
       <div className='book'>
         <div className='book-top'>
-          <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+          <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url("${img}")` }}></div>
           <div className='book-shelf-changer'>
             <select>
               <option value='move' disabled>Move to...</option>
@@ -22,8 +22,11 @@ class Book extends React.Component {
             </select>
           </div>
         </div>
-        <div className='book-title'>To Kill a Mockingbird</div>
-        <div className='book-authors'>Harper Lee</div>
+        <div className='book-title'>{title}</div>
+        {/* NOTE should authors be a list? most books only have one author, but for the ones that have multiples... */}
+        {authors.map(author => (
+          <div className='book-authors' key={author}>{author}</div>
+        ))}
       </div>
     )
   }
